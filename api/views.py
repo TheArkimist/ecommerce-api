ca1402dffe18754e6a601ee5e1ccba9e9a257120
Ci$c0 <emmanueloduro37@gmail.com>
2025-12-03 22:09:51 +0000

    Configured permissions for the Product and Category generic views

--- api/views.py
+++ api/views.py
@@ -2,23 +2,29 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import CategoryModel, ProductModel
 from .serializers import CategorySerializer, ProductSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 class ProductViewSet(ListCreateAPIView):
     queryset = ProductModel.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class ProductDetailViewSet(RetrieveUpdateDestroyAPIView):
     queryset = ProductModel.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
 
 class CategoryViewSet(ListCreateAPIView):
     queryset = CategoryModel.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CategoryDetailViewSet(RetrieveUpdateDestroyAPIView):
     queryset = CategoryModel.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
 
 
 
