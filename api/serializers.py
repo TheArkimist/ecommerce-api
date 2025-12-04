4ef1626820ddc2b4cc9c6f20735745f083dfbcf0
Ci$c0 <emmanueloduro37@gmail.com>
2025-12-04 12:01:07 +0000

    Implemented PageNumber Pagination for 18 products per page

--- api/serializers.py
+++ api/serializers.py
@@ -4,5 +4,5 @@ from .models import ProductModel
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductModel
-        fields = ['id', 'name', 'category', 'customer', 'description', 'stock_quantity', 'price', 'image_url', 'in_stock']
+        fields = ['id', 'name', 'category', 'description', 'stock_quantity', 'price', 'image_url', 'in_stock']
 
