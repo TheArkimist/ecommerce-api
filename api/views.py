b531a9a05982656e6cbdb9863c19b1d18b413439
Ci$c0 <emmanueloduro37@gmail.com>
2025-12-04 16:18:11 +0000

    Implemented Ordering by name

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from .models import ProductModel
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Create your views here.
 class ProductViewSet(ListCreateAPIView):
@@ -12,8 +12,9 @@ class ProductViewSet(ListCreateAPIView):
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     authentication_classes = [TokenAuthentication]
-    filter_backends = [SearchFilter]
+    filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['name', 'category__name']
+    ordering_fields = ['name']
 
 class ProductDetailViewSet(RetrieveUpdateDestroyAPIView):
     queryset = ProductModel.objects.all()
