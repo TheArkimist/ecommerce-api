29fa4e66f29c321cb6f5393a1ddb1bbdcddf9832
Ci$c0 <emmanueloduro37@gmail.com>
2025-12-04 16:04:12 +0000

    Configured Search Filter using product name and category

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from .models import ProductModel
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import SearchFilter
 
 # Create your views here.
 class ProductViewSet(ListCreateAPIView):
@@ -11,6 +12,8 @@ class ProductViewSet(ListCreateAPIView):
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     authentication_classes = [TokenAuthentication]
+    filter_backends = [SearchFilter]
+    search_fields = ['name', 'category__name']
 
 class ProductDetailViewSet(RetrieveUpdateDestroyAPIView):
     queryset = ProductModel.objects.all()
